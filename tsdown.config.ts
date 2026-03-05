6c2d99041fb1a29d3c90ec547ceca909462c8f38
Jayden <a8456452@naver.com>
2026-03-05 14:48:59 +0900

    feat: release 0.3.2
    - footer 레이아웃 수정
    - client 폴더 위치 변경

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'tsdown';
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: [
-    './src/index.ts',
-    './src/util/index.ts',
-    './src/ui/index.ts',
-    './src/ui/client-only/index.ts',
-    './src/hooks/index.ts',
-  ],
-  format: ['cjs', 'esm'],
+  entry: ["./src/index.ts", "./src/util/index.ts", "./src/ui/index.ts", "./src/ui-client/index.ts", "./src/hooks/index.ts"],
+  format: ["cjs", "esm"],
   dts: true,
   minify: true,
-  external: ['react', 'react-dom', 'react/jsx-runtime'],
+  external: ["react", "react-dom", "react/jsx-runtime"],
 });
